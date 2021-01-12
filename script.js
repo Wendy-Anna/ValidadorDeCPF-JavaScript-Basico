972b8d9c1c8e9e9ee5af653e1349b897e30c0995
Wendy-Anna <wendyufpb@gmail.com>
2021-01-12 14:06:14 -0300

    Primeiros passos

---
+++ script.js
@@ -0,0 +1,15 @@
+function validaCPF(cpf) {
+  return false;
+}
+
+function validacao() {
+  console.log("Iniciando validação de CPF");
+  var cpf = document.getElementById('cpf_digitado').value;
+  var resultadoValidacao = validaCPF(cpf);
+
+  if (resultadoValidacao) {
+    document.getElementById('success').style.display = 'block';
+  } else {
+    document.getElementById('error').style.display = 'block';
+  }
+}
